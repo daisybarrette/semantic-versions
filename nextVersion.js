e9fe22721300254647fab900e3b385e43314f322
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-11-10 13:48:16 -0500

    Add notes, add tests based on sample input/output

--- nextVersion.js
+++ nextVersion.js
@@ -1,7 +1,21 @@
 function nextVersion(currentVersion) {
-    // placeholder
+    // Assuming input is a well-formed version number, delimited by periods
+    const currentVersionArray = currentVersion.split('.');
 
-    console.log('current version is: ', currentVersion);
+    console.log('currentVersionArray ', currentVersionArray);
+
+    const nextVersionArray = currentVersionArray.map((num) => parseInt(num));
+
+    console.log(nextVersionArray);
+
+    console.log('nextVersionArray ', nextVersionArray);
+
+    // the last element is always incremented
+    // if it's between 0 and 8, add 1
+    // if it's 9, roll over to 0, and attempt to increment the next element
+
+    // loop through elements, possibly use recursion?
+
     return currentVersion;
 }
 
--- nextVersion.test.js
+++ nextVersion.test.js
@@ -3,3 +3,19 @@ const nextVersion = require('./nextVersion');
 test.skip('correctly increments 1.2.3', () => {
     expect(nextVersion('1.2.3')).toBe('1.2.4');
 });
+
+test.skip('correctly increments 0.9.9', () => {
+    expect(nextVersion('0.9.9')).toBe('1.0.0');
+});
+
+test.skip('correctly increments 1', () => {
+    expect(nextVersion('1')).toBe('2');
+});
+
+test.skip('correctly increments 1.2.3.4.5.6.7.8', () => {
+    expect(nextVersion('1.2.3.4.5.6.7.8')).toBe('1.2.3.4.5.6.7.9');
+});
+
+test.skip('correctly increments 9.9', () => {
+    expect(nextVersion('9.9')).toBe('10.0');
+});
