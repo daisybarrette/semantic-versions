3ce048f6d9c87c929b4d4299d72f6e19ec0b6acb
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-11-10 13:37:08 -0500

    Take current version from command line

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const nextVersion = require('./nextVersion');
 
 function main() {
-    nextVersion('3.7.1');
-    console.log('placeholder for main function');
+    const args = process.argv;
+    console.log(args);
+
+    // ASSUMPTION: Exactly one valid string argument is being passed via the command line
+    const currentVersion = args[2];
+
+    const result = nextVersion(currentVersion);
+
+    console.log('The current version is: ', currentVersion);
+    console.log('The next version is: ', result);
 }
 
 main();
